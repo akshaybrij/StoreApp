96e14b40999860ab023077fa1af55ef8d2f20c96
Akshay_Brijpuria_ <akshay.brijpuria@gmail.com>
2018-01-24 17:09:44 +0530

    Commit

--- src/app/component/midware/midware.component.ts
+++ src/app/component/midware/midware.component.ts
@@ -2,18 +2,22 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {DetailsService} from '../../service/details.service';
 import {Data} from '../../models/Data'; 
 import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 import '../../../../../psoi-22Jan/js/proton.js';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-midware',
   templateUrl: './midware.component.html',
   styleUrls: ['./midware.component.css']
 })
 export class MidwareComponent implements OnInit {
- rid:string;
+ rid:number;
  data:Data[];
  styleData:any;
+ balance:number; 
  message:string;
-  constructor(private detailservice:DetailsService,private router:Router) {
+  constructor(private detailservice:DetailsService,private router:Router,private toastrService:ToastrService,
+  private http:HttpClient) {
  
    }
 
@@ -29,11 +33,19 @@ export class MidwareComponent implements OnInit {
   }
   @Output() messageEvent = new EventEmitter<string>();
   onSubmit(){
+    //console.log(parseInt(this.rid);
   this.detailservice.getJson(this.rid).subscribe(response => {
    // alert(response);
    if(response==null){
-     alert("No User with this data");
-     //document.getElementsByClassName('main-grid')[0].className="hidden";
+    document.getElementsByClassName('error-message')[0].innerHTML=`<div class="alert alert-danger">
+    <strong>Wrong!</strong> No User Found with this RFID.
+  </div>
+  `
+  // document.getElementsByClassName('main-grid')[0].className="hidden";
+   setTimeout(function(){
+    window.location.reload()
+   },3000);
+
    }
    else{
     this.data=response
@@ -47,6 +59,13 @@ export class MidwareComponent implements OnInit {
   Logmeout(){
     localStorage.clear();
     this.router.navigate(['/']);
+  }
+  Increment(){
+  this.http.get("http://localhost/dsoi_new/api/mem_bal?rid="+this.rid+"&bal="+this.balance).subscribe(respond=>{
+    this.toastrService.success('Success', '', {
+      timeOut: 3000,
+    }
+  });
   }
 }
 
